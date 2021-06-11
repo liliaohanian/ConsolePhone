7f507e11db533c1829b87bf563a40957556505c9
liliaohanian <72820128+liliaohanian@users.noreply.github.com>
2021-06-11 11:45:10 +0400

    Add project files.

---
+++ ConsolePhone/Phone.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePhone
+{
+    public class Phone
+    {
+        private int _number;
+        private string _model;
+        private int _weight;
+
+        public int Number { get; set; }
+        public string Model { get; set; }
+        public int Weight { get; set; }
+        public Phone(int number, string model, int weight ): this(number, model)
+        {
+            Number = number;
+            Model = model;
+            Weight = weight;
+        }
+        public Phone(int number, string model)
+        {
+            Number = number;
+            Model = model;
+        }
+        
+        public void ReceiveCall(string callerName)
+        {
+            Console.WriteLine($"{callerName} - {Number} is calling.");
+        }
+        public void SendMessage(int phoneNmubers)
+        {
+            Console.WriteLine($"Phone number: {phoneNmubers}");
+        }
+
+    }
+}
---
+++ ConsolePhone/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsolePhone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone1 = new Phone(99099099, "MS240", 23);
+            Phone phone2= new Phone(99099099, "MS240", 23);
+            Phone phone3 = new Phone(99099099, "MS240", 23);
+            Phone phone4 = new Phone(99099099, "MS240", 23);
+            phone1.ReceiveCall("Jack");
+            phone1.SendMessage(011545676);
+            Phone phone = new Phone(011343333, "Iphone");
+            phone.SendMessage(010222333);
+        }
+    }
+}
